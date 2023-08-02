567b24e509610387805fb916c4f2427fb2021a3d
Herald <herald.balerite.work@gmail.com>
2023-08-02 01:46:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res){
-	res.send("Hello Everyone - Learn to use Github Actions - github deploy docker to ec2");
+	res.send("Hello Everyone! Learn to use Github Actions and deploy docker to Amazon EC2");
 });
 
 app.listen( port, function() {
